bc3d1dcd2795a51f88126e2cf86c3ea87a7b0517
Lim Chee Keat <cheekeat@mailer.blueoceanit.com.my>
2021-06-12 00:11:05 +0800

    Update article scroll effect

--- src/app/pages/home/article-card/article-card.component.ts
+++ src/app/pages/home/article-card/article-card.component.ts
@@ -1,47 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-article-card',
+  selector: 'div[app-article-card]',
   templateUrl: './article-card.component.html',
   styleUrls: ['./article-card.component.scss']
 })
 export class ArticleCardComponent implements OnInit {
-
+  @Input('articleObj') article!: any;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  article = [
-    {
-      id: 1,
-      type: 'tips',
-      title: 'How to Pick The Perfect Peach',
-      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
-    },
-    {
-      id: 2,
-      type: 'lifestyle',
-      title: 'Minimalist Kitchen',
-      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
-    },
-    {
-      id: 3,
-      type: 'recipe',
-      title: 'Butternut Squash Soup',
-      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
-    },
-    {
-      id: 4,
-      type: 'recipe',
-      title: 'Berries Smoothie',
-      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
-    },
-    {
-      id: 5,
-      type: 'tips',
-      title: 'Organise Your Space',
-      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
-    },
-  ]
+  
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild ("articleCard", { static: false }) private articleCard!: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  moveLeft() {
+    this.articleCard.nativeElement.scrollBy(-350, 0);
+  }
+  
+  moveRight() {
+    this.articleCard.nativeElement.scrollBy(350, 0);
+  }
+
+  article = [
+    {
+      id: 1,
+      type: 'tips',
+      title: 'How to Pick The Perfect Peach',
+      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
+    },
+    {
+      id: 2,
+      type: 'lifestyle',
+      title: 'Minimalist Kitchen',
+      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
+    },
+    {
+      id: 3,
+      type: 'recipe',
+      title: 'Butternut Squash Soup',
+      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
+    },
+    {
+      id: 4,
+      type: 'recipe',
+      title: 'Berries Smoothie',
+      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
+    },
+    {
+      id: 5,
+      type: 'tips',
+      title: 'Organise Your Space',
+      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
+    },
+  ]
 }
