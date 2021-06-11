e2d3fee5cdc1dd3278117a50de3f88ec88d5ea53
Lim Chee Keat <cheekeat@mailer.blueoceanit.com.my>
2021-06-11 10:17:21 +0800

    Update home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title = 'Blog';
+  darkMode: boolean = false;
+  constructor(){
+
+  }
+  ngOnInit() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TopNavBarComponent } from './pages/top-nav-bar/top-nav-bar.component';
+import { ArticleCardComponent } from './pages/home/article-card/article-card.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopNavBarComponent,
+    ArticleCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
