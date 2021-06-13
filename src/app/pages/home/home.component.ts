fba36d01fd028275a6ecf5a3e2825fe832d59c0f
Lim Chee Keat <cheekeat@mailer.blueoceanit.com.my>
2021-06-13 23:10:47 +0800

    -Update home main slide animation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ViewChildren, ElementRef, QueryList, ɵɵsetComponentScope } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,17 +7,70 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  @ViewChild ("articleCard", { static: false }) private articleCard!: ElementRef;
+  @ViewChild("articleCard") private articleCard!: ElementRef;
+  @ViewChildren("slideShow") private slideShow!: QueryList<any>;
+  @ViewChildren("dot") private dot!: QueryList<any>;
+  slideShowArr: ElementRef[] = [];
+  dotArr: ElementRef[] = [];
+  selectedSlide: number = 0;
+  timer: any;
   constructor() { }
 
   ngOnInit(): void {
+    // this.refreshSlide(this.selectedSlide);
   }
 
-  moveLeft() {
+  ngAfterViewInit() {
+    this.slideShowArr = this.slideShow.toArray();
+    this.dotArr = this.dot.toArray();
+    this.refreshSlide();
+  }
+
+  refreshSlide() {
+    if (this.selectedSlide > this.slideShowArr.length - 1) {
+      this.selectedSlide = 0
+    };
+    if (this.selectedSlide < 0) {
+      this.selectedSlide = this.slideShowArr.length - 1
+    };
+    this.slideShowArr.forEach(slide => {
+      slide.nativeElement.style.display = "none";
+    });
+    this.dotArr.forEach(dot => {
+      dot.nativeElement.className = "";
+    });
+    console.log(this.dotArr);
+    this.slideShowArr[this.selectedSlide].nativeElement.style.display = "flex";
+    this.dotArr[this.selectedSlide].nativeElement.className += "intro__dot__active";
+    this.timer = setTimeout(() => {
+      this.selectedSlide += 1;
+      this.refreshSlide();
+    }, 10000);
+  }
+
+  introPrevious() {
+    clearTimeout(this.timer);
+    this.selectedSlide -= 1;
+    this.refreshSlide();
+  }
+  
+  introNext() {
+    clearTimeout(this.timer);
+    this.selectedSlide += 1;
+    this.refreshSlide();
+  }
+
+  chgSlide(index: number) {
+    clearTimeout(this.timer);
+    this.selectedSlide = index;
+    this.refreshSlide();
+  }
+
+  articleMoveLeft() {
     this.articleCard.nativeElement.scrollBy(-350, 0);
   }
   
-  moveRight() {
+  articleMoveRight() {
     this.articleCard.nativeElement.scrollBy(350, 0);
   }
 
@@ -52,5 +105,16 @@ export class HomeComponent implements OnInit {
       title: 'Organise Your Space',
       description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumt eirmod tempor invidunt ut labore er dolor',
     },
+  ]
+
+  slides = [
+    {
+      id: 1,
+      title: 'home_bg1.jpg'
+    },
+    {
+      id: 2,
+      title: 'home_bg2.jpg'
+    }
   ]
 }
